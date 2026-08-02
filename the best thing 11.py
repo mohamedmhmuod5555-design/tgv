fd8244bde9efc1b4172b83b9290219bb246cce7a
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-02 15:51:44 -0700

    Update the best thing 11.py

--- the best thing 11.py
+++ the best thing 11.py
@@ -87,7 +87,7 @@ if st.session_state.hearts ==0:
  if st.session_state.coins>=3:
   if st.button("انقاذ النقاط "): 
     st.session_state.coins -=3
-    st.session_state.num= st.session_state.c-3
+    st.session_state.num= st.session_state.c
     st.session_state.hearts=3
     time.sleep(1)
     st.rerun()
