4963909697e5386e8ef5c6eecabc0f3ccf6a81b6
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-02 16:34:48 -0700

    Update the best thing 11.py

--- the best thing 11.py
+++ the best thing 11.py
@@ -35,6 +35,8 @@ if 'hearts' not in st.session_state:
  st.session_state.hearts=3
 if 'coins' not in st.session_state:
  st.session_state.coins=0
+if 'c' not in st.session_state:
+ st.session_state.c=0
 
 
 
@@ -57,6 +59,7 @@ if st.button("تأكيد التخمين "):
   if number == sc:
    st.session_state.num += 1
    st.session_state.coins +=1
+    st.session_state.c+=1
    st.session_state.feed="correct"
   else:
    st.session_state.hearts-=1
@@ -82,7 +85,7 @@ if st.session_state.hearts ==0:
  st.error("للاسف انتهت المحاولات ان كنت تريد انقاذ يمكن الحفاظ عليها ببيع ثلاث عملات ذهبيه ") 
  if st.session_state.coins>=3:
   if st.button("انقاذ النقاط "): 
-    st.session_state.coins -=3
+    st.session_state.c
     st.session_state.hearts=3
     time.sleep(1)
     st.rerun()
