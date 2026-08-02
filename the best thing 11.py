3f5220e648af293a59fa5eaf6092de7ca2547ed1
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-02 15:38:29 -0700

    Update the best thing 11.py

--- the best thing 11.py
+++ the best thing 11.py
@@ -84,13 +84,14 @@ if st.session_state.hearts ==0:
   if st.button("انقاذ النقاط "): 
     st.session_state.coins -=3
     st.session_state.hearts=3
-    
-  else:
-     st.error("للاسف ليس لديك ما يكفي من العملات حتي تحا فظ علي نقاطك ")
-     st.session_state.num=0
-     st.session_state.hearts=3
-     time.sleep(2)
-     st.rerun()
+    time.sleep(1)
+    st.rerun()
+ else:
+    st.error("للاسف ليس لديك ما يكفي من العملات حتي تحا فظ علي نقاطك ")
+    st.session_state.num=0
+    st.session_state.hearts=3
+    time.sleep(2)
+    st.rerun()
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
   if st.button("الليفل التالي "):
